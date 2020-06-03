1fae968326aaa2263382e152f87cbdc912f73db7
asuradev99 <ethanaditya@gmail.com>
2020-06-03 14:37:53 -0600

    added Page classes for basic page requesting functionality

---
+++ include/Page.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Page {
+    private:
+        const std::string url, title, created_at, updated_at, editing_roles, last_edited_by, published, front_page, locked_for_user, lock_info, lock_explanation;
+    public:
+
+};
