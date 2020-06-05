224f8709f5535d60884292ff7aea7ed3179bd1fc
asuradev99 <ethanaditya@gmail.com>
2020-06-05 17:53:42 -0600

    minor changes

--- include/Page.hpp
+++ include/Page.hpp
@@ -1,20 +1,23 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <map>
 
 class Page {
     private:
-        const std::string url, 
-                          title, 
-                          created_at, 
-                          updated_at, 
-                          editing_roles, 
-                          last_edited_by, 
-                          published, 
-                          front_page, 
-                          locked_for_user, 
-                          lock_info, 
-                          lock_explanation;
+        std::string url, 
+                    title, 
+                    created_at, 
+                    updated_at, 
+                    editing_roles, 
+                    last_edited_by, 
+                    published, 
+                    front_page, 
+                    locked_for_user, 
+                    lock_info, 
+                    lock_explanation;
+        std::string raw_body;
+        std::map <std::string, std::string> fmt_header;
     public:
         
 
