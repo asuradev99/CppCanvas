581ba67b9f57813d66d64fbc6d547d725bc4e429
asuradev99 <ethanaditya@gmail.com>
2020-06-05 17:52:25 -0600

    Added single page functionality

--- include/Canvas.hpp
+++ include/Canvas.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string> 
 #include "RequestHandler.hpp"
+#include "HttpObject.hpp"
 #include "PageList.hpp"
 #include "Page.hpp"
 class Canvas 
@@ -12,37 +13,22 @@ class Canvas
     public:
     //overloaded constructors
         Canvas () {}
-        Canvas (const std::string& _token)
+        Canvas (const std::string& _token, const std::string& _url) :
+        token(_token), base_url(_url)
         {
-            SetToken (_token);
-        }
-        Canvas (const std::string& _url)
-        {
-            SetUrl (_url);
-        }
-        Canvas (const std::string& _token, const std::string& _url)
-        {
-            SetToken(_token);
-            SetUrl (_url);
-        }
-    //set authentication token 
-        void SetToken (const std::string& _token)
-        {
-            token = _token;
-        }
-    //set url
-        void SetUrl (const std::string& _url)
-        {
-            base_url = _url;
+    
         }
     //member functions 
         PageList GetPagesList (const std::string& _url)
         {
             
         }
-        Page GetPage (const std::string& _url)
+        void GetPage (const std::string& _url)
         {
+            RequestHandler requests (base_url, token);
+            HttpObject response = requests.GetRequest(_url, true, true);
+            
             
         }
-
+        
 };
