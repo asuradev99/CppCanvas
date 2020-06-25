e75160d97d972e7261ae0d3c6b5a4ad5b070dbc0
asuradev99 <ethanaditya@gmail.com>
2020-06-25 17:55:37 -0600

    major changes and updates to the parsing system, fixed json type error

--- include/Page.hpp
+++ include/Page.hpp
@@ -15,27 +15,30 @@ class Page {
                     created_at, 
                     updated_at, 
                     editing_roles, 
-                    last_edited_by, 
-                    published, 
-                    front_page, 
-                    locked_for_user, 
-                    lock_info, 
-                    lock_explanation;
-        std::string raw_body;
+                    last_edited_by_name,
+                    lock_info,
+                    lock_explanation,
+                    raw_body;
+        int id, last_edited_by_id;
+        bool locked_for_user, 
+             published,
+             front_page;
         std::map <std::string, std::string> fmt_header;
-        json fmt_body;
 
         Page (const HttpObject& http) :
             fmt_header (http.fmt_header), raw_body (http.GetRawBody()) 
         {
             try {
-                fmt_body = json::parse (raw_body);
+                json fmt_body = json::parse (raw_body);
+
                 if (fmt_body.contains("message")) 
                 {
                     throw api_errors::PageNotFound();
+                    return;
                 } else {
-                    std::cout << fmt_body << "\n";
+
                     url = fmt_body["html_url"];
+                    id = fmt_body["page_id"];
                     title = fmt_body["title"];
                     created_at = fmt_body["created_at"];
                     updated_at = fmt_body["updated_at"];
@@ -45,22 +48,29 @@ class Page {
                     locked_for_user = fmt_body["locked_for_user"];
 
                     if (locked_for_user)
+                    {
                         lock_info = fmt_body["lock_info"];
                         lock_explanation = fmt_body["lock_explanation"];
-
-                    if (fmt_body.contains("last_edited_by")) 
-                        last_edited_by = fmt_body["last_edited_by"];
+                    }
+                    if (fmt_body.contains("last_edited_by"))
+                    {
+                        last_edited_by_name = fmt_body["last_edited_by"]["display_name"];
+                        last_edited_by_id = fmt_body["last_edited_by"]["id"];
+                    }
+                        
                     
                 }
             } catch (const std::exception& e) {
                 std::cerr << e.what() << "\n";
             }
             
-            
-            
         }
         
 };
+class PageRevision {
+    public: 
+        
+}
 struct PageOptions {
      
 };
