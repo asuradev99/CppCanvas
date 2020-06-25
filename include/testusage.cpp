268ca8b321d42968047eeac45476b29050f70c10
asuradev99 <ethanaditya@gmail.com>
2020-06-25 17:56:07 -0600

    updated example

--- include/testusage.cpp
+++ include/testusage.cpp
@@ -2,13 +2,11 @@
 //compiles with g++ testusage.cpp -lcurl -lcurlpp
 int main (int argc, char* argv[]) 
 {
-    std::cerr << "Test error";
-    std::cout << argc << '\n';
+ 
     std::string key = argv[1];
-    std::string url = "https://graniteschools.instructure.com/api/v1/";
+    std::string url = "https://canvas.instructure.com/api/v1/";
     Canvas canvas (key,url);
-    Page page = canvas.GetPage(std::string ("courses/1337964/pages/aug-26-slash-27"));
-    std::cout << page.title << "\n";
+    Page page = canvas.GetPage(std::string ("courses/2120073/pages/sample-page"));
 
 }
 //test commit
