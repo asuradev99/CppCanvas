a5ea1413bf37e7f4f63147de79613b809df0d496
asuradev99 <ethanaditya@gmail.com>
2020-06-25 19:01:16 -0600

    major refactoring changes

--- include/Page.hpp
+++ include/Page.hpp
@@ -7,70 +7,47 @@
 #include "CanvasExceptions.hpp"
 
 typedef nlohmann::json  json;
+enum class PageAttributes : std::string {
+    url = "html_url",
+    id = "page_id",
+    title = "title",
+    body = "body",
+    created_at = "created_at",
+    updated_at = "updated_at",
+    editing_roles = "editing_roles",
+    published = "published",
+    front_page = "front_page",
+    locked_for_user = "locked_for_user",    
+};
 
 class Page {
-    public:
-        std::string url, 
-                    title, 
-                    created_at, 
-                    updated_at, 
-                    editing_roles, 
-                    last_edited_by_name,
-                    lock_info,
-                    lock_explanation,
-                    raw_body;
-        int id, last_edited_by_id;
-        bool locked_for_user, 
-             published,
-             front_page;
-        std::map <std::string, std::string> fmt_header;
-
+    private:
+        std::map <std::string, std::string> parsed_header;
+        std::string raw_body;
+        json json_body;
+    public: 
         Page (const HttpObject& http) :
-            fmt_header (http.fmt_header), raw_body (http.GetRawBody()) 
+            parsed_header (http.fmt_header), raw_body (http.GetRawBody()) 
         {
             try {
-                json fmt_body = json::parse (raw_body);
+                json_body = json::parse (raw_body);
 
-                if (fmt_body.contains("message")) 
+                if (json_body.contains("message")) 
                 {
                     throw api_errors::PageNotFound();
                     return;
-                } else {
-
-                    url = fmt_body["html_url"];
-                    id = fmt_body["page_id"];
-                    title = fmt_body["title"];
-                    created_at = fmt_body["created_at"];
-                    updated_at = fmt_body["updated_at"];
-                    editing_roles = fmt_body["editing_roles"];
-                    published = fmt_body["published"];
-                    front_page = fmt_body["front_page"];
-                    locked_for_user = fmt_body["locked_for_user"];
-
-                    if (locked_for_user)
-                    {
-                        lock_info = fmt_body["lock_info"];
-                        lock_explanation = fmt_body["lock_explanation"];
-                    }
-                    if (fmt_body.contains("last_edited_by"))
-                    {
-                        last_edited_by_name = fmt_body["last_edited_by"]["display_name"];
-                        last_edited_by_id = fmt_body["last_edited_by"]["id"];
-                    }
-                        
-                    
                 }
             } catch (const std::exception& e) {
                 std::cerr << e.what() << "\n";
             }
             
         }
-        
+        auto get (const std::string& key) {
+            return json_body[key];
+        }
+  
 };
 class PageRevision {
     public: 
-        
-}
-struct PageOptions {
-     
+
 };
--- include/testusage.cpp
+++ include/testusage.cpp
@@ -7,6 +7,5 @@ int main (int argc, char* argv[])
     std::string url = "https://canvas.instructure.com/api/v1/";
     Canvas canvas (key,url);
     Page page = canvas.GetPage(std::string ("courses/2120073/pages/sample-page"));
-
 }
 //test commit
