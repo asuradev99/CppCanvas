e9351349a3d21138c523b2e87dc2e4a4b108bdb9
asuradev99 <ethanaditya@gmail.com>
2020-06-28 11:55:00 -0600

    directory change

--- include/PageList.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once 
-#include <iostream>
-#include <string>
-
-class PageList {
-    private:
-    public:
-};
--- include/Session.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-namespace CppCanvas 
-{
-    struct Session {
-        std::string token, url;
-    };
-}
-      
