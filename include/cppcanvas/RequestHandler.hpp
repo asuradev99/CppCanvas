2691258e28595728929a26fcc723d032758d199a
asuradev99 <ethanaditya@gmail.com>
2020-06-30 21:24:16 -0600

    updated POST interface

--- include/cppcanvas/RequestHandler.hpp
+++ include/cppcanvas/RequestHandler.hpp
@@ -21,6 +21,20 @@ namespace CppCanvas {
         }
         RequestOptions () {}
     };
+    struct PostOptions {
+        std::string url, opt;
+        bool include_hdr;
+        bool include_body;
+        bool PUT = false;
+        PostOptions (const std::string& _url, const std::vector<std::string> _params, const bool& hdr, const bool& body) :
+        url (_url), include_hdr(hdr), include_body(body) 
+        {
+            for (auto x : _params)
+            {
+                opt += (x + "&");
+            }
+        }
+    };
     class RequestHandler {
         private: 
             std::string token;
@@ -34,6 +48,7 @@ namespace CppCanvas {
             void SetUrl (const std::string& url)
             {
                 std::string full_url = base_url.append(url);
+                std::cout << full_url << "\n";
                 handler.setOpt(new cURLpp::Options::Url (full_url));
             }
             void IncludeContent(const bool& include_header, const bool& include_body)
@@ -60,6 +75,22 @@ namespace CppCanvas {
                 CppCanvas::HttpObject output (httpresponse.str());
                 return output;
             }  
+            void PostRequest (const PostOptions& options)
+            {
+                std::string auth_header = "Authorization: Bearer ";
+                auth_header.append(token);
+                std::list<std::string> headers {auth_header};
+              //  headers.push_back("Content-Type: application/json");
+
+                SetUrl (options.url);
+                IncludeContent(options.include_hdr,options.include_body);
+                handler.setOpt(new curlpp::options::HttpHeader(headers)); 
+                handler.setOpt(new curlpp::options::CustomRequest("PUT"));
+                std::cout << options.opt << "\n";
+
+                handler.perform();
+                handler.reset();
+            }
 
     };
 }
